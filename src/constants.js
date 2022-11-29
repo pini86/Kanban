1f8140daefa9d99ea07d613b1cf94c76d78fd507
Mikhail Druzhinin <pini86@mail.ru>
2022-11-29 19:07:02 +0300

    add

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SECRET_KEY = exports.PORT = void 0;
+exports.PORT = 3000;
+exports.SECRET_KEY = 'myVerySecretKey-04.05.2022';
+// mongodb+srv://MatsveiDubaleka:m29092003O@test-pma.qhaq1uv.mongodb.net/?retryWrites=true&w=majority - Test MongoDB
+// localhost:3000
